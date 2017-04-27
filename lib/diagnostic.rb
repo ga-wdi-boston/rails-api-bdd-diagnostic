6d7ed302d2d3cf191848d1481c6de86bea6921c5
jkjellan <jkjellan@gmail.com>
2017-04-27 09:24:27 -0400

    Adds responses to rails-api-bdd-diagnostic

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -19,7 +19,9 @@ end
 # In a Ruby comment, explain Behavior Driven Development, how it is meant to be
 # used, and how it differs from Test Driven Development.
 
-# your answer here
+# Behavior Driven Development is development is similar to TDD,
+# but Acceptance and Unit tests are focused around testing the fulfillment
+# of user stories
 
 #
 # Question 2
@@ -28,7 +30,11 @@ end
 # responds successfully and lists all examples.
 
 RSpec.describe 'Examples API' do
-  # your test(s) here
+  describe 'GET /examples' do
+  it 'lists all examples' do
+    get '/examples'
+
+    expect(response).to be_success
 end
 
 #
@@ -38,7 +44,8 @@ end
 # GET /examples/:id routes to the examples#show action.
 
 RSpec.describe 'routes for examples' do
-  # your test(s) here
+  it 'routes GET /examples to the examples#index action' do
+    expect(get('/examples')).to route_to('examples#index')
 end
 
 #
@@ -56,7 +63,16 @@ RSpec.describe ExamplesController do
   end
 
   describe 'POST create' do
-    # your test(s) here
+    before(:each) do
+      post :create, params: { example: example_params }
+    end
+    it 'is successful' do
+      expect(response).to be_success
+    end
+    it 'renders a JSON response' do
+      example_response = JSON.parse(response.body)
+      example(example_response).not_to be_nil
+    end
   end
 end
 
@@ -74,7 +90,13 @@ RSpec.describe ExamplesController do
   end
 
   describe 'PATCH update' do
-    # your test(s) here
+    before(:each) do
+      patch :update, params: { id: example.id, body: example_diff }
+    end
+
+    it 'is successful' do
+      expect(response).to be_success
+    end
   end
 end
 
@@ -90,7 +112,11 @@ RSpec.describe ExamplesController do
   end
 
   describe 'DELETE destroy' do
-    # your test(s) here
+    it 'is successful and returns an empty reponse' do
+      delete :destroy, params: { id: example.id }
+
+    expect(response).to be_successful
+    expect(response.body).to be_empty
   end
 end
 
@@ -104,7 +130,7 @@ end
 RSpec.describe Example do
   describe 'associations' do
     # association method here
-
+    # I don't know how to do this!
     # test association with `other` here
   end
 end
